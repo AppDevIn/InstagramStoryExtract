406b0596b46c66a39098ce1e0c3c3180aaac8938
AppDevIn <teamprojectlive@gmail.com>
2021-11-14 21:57:19 +0800

    Change print line to show number of downloads

--- script.py
+++ script.py
@@ -148,4 +148,4 @@ if __name__ == "__main__":
         driver.find_element_by_xpath(
             "/html/body/div[1]/section/div[1]/div/section/div/button[2]").click()
     driver.close()
-    print(imagesArr)
+    print(f"The number of image/video downloaded are ${len(imagesArr)}")
