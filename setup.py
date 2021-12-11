273e6a016e1dd865ce9fd747bdb6b7e4e4db661f
AppDevIn <teamprojectlive@gmail.com>
2021-12-11 21:58:19 +0800

    Added setup file for story modules, Updated .gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='InstagramScrap',
+   version='0.0.1',
+   author='Sliver',
+   author_email='jeyavishnu@gmail.com',
+   py_modules=['story_main', "src/Bot"]
+)
