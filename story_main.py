4b002987fa992a247f9e3517d201fb5a72fdb1cd
AppDevIn <teamprojectlive@gmail.com>
2021-12-12 02:12:46 +0800

    Bug fix: log_path to path

--- story_main.py
+++ story_main.py
@@ -75,7 +75,7 @@ def downloadFiles(stories: StoriesModel, profile_name, data_path, logger):
 
 def snapScreenshotOfProfile(bot, profile_name, path) -> str:
     bot.landProfilePage(profile_name)
-    screenshot_path = f"{log_path}/{datetime.now().strftime(DateUtil.DATE_FORMAT)}/screenshot_{profile_name}.png"
+    screenshot_path = f"{path}/{datetime.now().strftime(DateUtil.DATE_FORMAT)}/screenshot_{profile_name}.png"
     bot.takeScreenshot(".zw3Ow", screenshot_path)
     return screenshot_path
 
